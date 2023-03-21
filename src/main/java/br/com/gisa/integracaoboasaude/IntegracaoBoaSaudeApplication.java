f04c7b42c78aafca514f522fecdf748973b68c5d
Alcides <1287814@sga.pucminas.br>
2023-03-21 20:17:28 -0300

    initial commit

---
+++ src/main/java/br/com/gisa/integracaoboasaude/IntegracaoBoaSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.gisa.integracaoboasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoBoaSaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegracaoBoaSaudeApplication.class, args);
+    }
+
+}
